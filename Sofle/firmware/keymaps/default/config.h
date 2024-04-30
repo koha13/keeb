567675acc372799e2362230b9cc9d32228337ab1
Khoatd <khoa.trandang@fssc.com.vn>
2024-04-30 17:10:56 +0700

    them firmware sofle

---
+++ Sofle/firmware/keymaps/default/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define OLED_TIMEOUT 60000
+#define OLED_BRIGHTNESS 120
+#ifndef TAPPING_TERM
+#    define TAPPING_TERM 400
+#endif // TAPPING_TERM
+#define ONESHOT_TIMEOUT 500
+#define ONESHOT_TAP_TOGGLE 5
+#define PERMISSIVE_HOLD
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
